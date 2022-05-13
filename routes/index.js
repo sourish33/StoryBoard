6e8ca1343eeda38282e59bfbad69f250bcd0a0b0
Sourish Dutta <sourishdutta@Sourishs-MacBook-Pro.local>
2022-05-13 13:39:46 +0530

    title variable added

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,13 @@ const router = express.Router()
 //  @desc Login/Landing page
 // @route GET /
 router.get('/', (req, res)=>{
-    res.render('main.ejs', {name: "Sourish Dutta"})
+    res.render('main.ejs', {name: "Sourish Dutta", title: "Home"})
 })
 
 // @desc Dashboard
 //@route  GET /dashboard
 router.get('/dashboard', (req, res) => {
-    res.render('dashboard.ejs', {adj: "Pinche"})
+    res.render('dashboard.ejs', {adj: "fucking", title: "Dashboard"})
 })
 
 module.exports = router
