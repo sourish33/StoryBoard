b7d4133e3731edd99e1bf14e4ed56be6d0f6d152
SD <sourish.d@gmail.com>
2022-05-21 21:32:02 +0530

    Edit page working

--- routes/stories.js
+++ routes/stories.js
@@ -1,30 +1,25 @@
-const express = require('express')
-const { processText, formatTime } = require('../helpers')
+const express = require("express")
+const { processText, formatTime } = require("../helpers")
 const router = express.Router()
-const {ensureAuth} = require('../middleware/auth')
-const Story= require('../models/Story')
-const User = require('../models/User')
-
+const { ensureAuth } = require("../middleware/auth")
+const Story = require("../models/Story")
+const User = require("../models/User")
 
 //  @desc Show Add page
 // @route GET /
 
-
-
-
-router.get('/add', ensureAuth,(req,res)=>{
-    res.render('./partials/add.ejs')
+router.get("/add", ensureAuth, (req, res) => {
+    res.render("./partials/add.ejs")
 })
 
-router.post('/', ensureAuth, async (req, res)=>{
+router.post("/", ensureAuth, async (req, res) => {
     try {
         req.body.user = req.user.id
         await Story.create(req.body)
-        res.redirect('/dashboard')
+        res.redirect("/dashboard")
     } catch (error) {
         console.error(error)
-        res.render('./error/500.ejs')
-        
+        res.render("./error/500.ejs")
     }
 })
 
@@ -33,26 +28,38 @@ stories which are public
 title, body, user.name, user.image
 
 */
-router.get('/', ensureAuth,async (req,res)=>{
+router.get("/", ensureAuth, async (req, res) => {
     try {
-        const retrievedStories = await Story.find(
-            {status: "public"}
-            ).sort({'createdAt': -1}).lean().populate('user').exec()
-        retrievedStories.forEach(story => {
+        const retrievedStories = await Story.find({ status: "public" })
+            .sort({ createdAt: -1 })
+            .lean()
+            .populate("user")
+            .exec()
+        retrievedStories.forEach((story) => {
             story.body = processText(story.body, 200)
             story.title = processText(story.title, 25)
-            story.editIcon = story.user.googleId === req.user.googleId ? "" : "hidden"
+            story.editIcon =
+                story.user.googleId === req.user.googleId ? "" : "hidden"
             story.storyID = story._id
             story.createdAt = formatTime(story.createdAt)
         })
-        res.render('./stories/index.ejs', {retrievedStories: retrievedStories})
-        
+        res.render("./stories/index.ejs", {
+            retrievedStories: retrievedStories,
+        })
     } catch (error) {
         console.log(error)
     }
-
-    
 })
 
+router.get("/edit/:id", ensureAuth, async (req, res) =>{
+    const story = await Story.findOne({_id: req.params.id}).populate("user").lean()
+    if(!story){
+        return res.render("error/404")
+    }
+    if (story.user.googleId !== req.user.googleId){
+        return res.redirect("/stories")
+    }
+    res.render("./partials/edit.ejs", {story: story})
+})
 
 module.exports = router
