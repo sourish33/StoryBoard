8092329af028a8798e8d3482e84bb0b5a6404335
SD <sourish.d@gmail.com>
2022-05-29 11:14:44 -0500

    registering user by email works

--- models/User.js
+++ models/User.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 const UserSchema = new mongoose.Schema({
     googleId: {
         type: String,
-        required: true
     },
     displayName: {
         type: String,
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,9 @@
 const express = require("express")
+const bcrypt= require('bcrypt')
 const router = express.Router()
 const passport = require("passport")
+const { ensureGuest } = require("../middleware/auth")
+const User = require("../models/User")
 
 //  @desc Auth with google
 // @route GET /auth/google
@@ -20,6 +23,24 @@ router.get(
 router.get('/logout', (req, res) => {
     req.logout()
     res.redirect('/')
+})
+
+router.post("/register", ensureGuest, async (req, res)=>{
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        const user = {
+            displayName: `${req.body.firstName} ${req.body.lastName}`,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            password:req.body.password
+        }
+        await User.create(user)
+        res.redirect("/dashboard")
+    } catch (err) {
+        res.render("./error/500.ejs", {error: err})
+    }
+
 })
 
 module.exports = router
