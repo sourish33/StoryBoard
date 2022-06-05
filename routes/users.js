a30670b5a83d94491e7234e21ad2febc98fa1152
SD <sourish.d@gmail.com>
2022-06-05 15:07:41 -0500

    addRemoveLike route created successfully returns data

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,48 @@ const Story = require("../models/Story")
 const User = require("../models/User")
 
 
+//add or remove a like
+router.patch('/addRemoveLike', ensureAuth, async (req, res) =>{
+    console.log("Hello from addRemoveLike!!!")
+    const userid = req.user._id
+    const storyID = req.body.storyID
+    // console.log(`Author: ${req.user._id}`)
+    // console.log(`userid = ${userid}`)
+    // console.log(`storyid = ${storyID}`)
+    try {
+        const theUser = await User.findById(userid).lean()
+        const theUserLiked = theUser.liked.map(el=>el.toString())
+        // console.log(theUserLiked)
+        let likeButtonBig = true
+        if (theUserLiked.includes(storyID)) {
+            const updatedUser = await User.updateOne(
+                {_id: userid}, 
+                { $pull: { liked: storyID } }
+            )
+            likeButtonBig = false
+        } else{
+            const updatedUser = await User.updateOne(
+                {_id: userid}, 
+                { $addToSet: { liked: storyID } }
+            )
+        }
+        //get updated number of likes
+        const numLikes = await User.find({liked: storyID}).countDocuments()
+
+        //prepare data to send back
+        const data = {
+            likeButtonBig: likeButtonBig,
+            numLikes: numLikes
+        }
+        console.log(data)
+        res.status(200).render('./error/output.ejs', {data: data})
+
+    } catch (err) {
+        res.status(404).render("./error/500.ejs", {error: err})
+    }
+    
+    
+}) 
 
 
 //add a story to a users list of liked stories
@@ -37,10 +79,14 @@ router.patch("/addlike", ensureAuth, async (req, res)=>{
     }
 })
 
+//doesn't do anythng useful
 router.get("/test", ensureAuth, async (req, res)=>{
     console.log("Hello from test")
     console.log(req.user)
-    res.redirect("/stories")
+    res.status(200).send({
+        status: "OK",
+        data: "everything good"
+    })
 })
 
 router.get("*", (req, res) => {
@@ -51,6 +97,5 @@ router.get("*", (req, res) => {
   });
 
 
-// remove a story from a users list of like stories
 
 module.exports = router
