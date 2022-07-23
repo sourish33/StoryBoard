12655b26c1619b571255daf16ab9b85e4ee8afb8
SD <sourish.d@gmail.com>
2022-07-23 15:43:44 -0500

    Search option You Liked added

--- routes/stories.js
+++ routes/stories.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const { route } = require(".")
-const { processText, formatTime, formatTimeDateOnly, } = require("../helpers")
+const { processText, formatTime, formatTimeDateOnly } = require("../helpers")
 const router = express.Router()
 const { ensureAuth } = require("../middleware/auth")
 const Story = require("../models/Story")
@@ -13,7 +13,7 @@ router.get("/add", ensureAuth, (req, res) => {
     res.render("./partials/add.ejs")
 })
 
-//@desc saving a new story 
+//@desc saving a new story
 router.post("/", ensureAuth, async (req, res) => {
     try {
         req.body.user = req.user._id
@@ -21,7 +21,7 @@ router.post("/", ensureAuth, async (req, res) => {
         res.redirect("/dashboard")
     } catch (error) {
         console.log(error)
-        res.render("./error/500.ejs", {error:error})
+        res.render("./error/500.ejs", { error: error })
     }
 })
 
@@ -38,50 +38,54 @@ router.post("/:id", ensureAuth, async (req, res) => {
 })
 
 //Middleware to retrieve stories and likes
-const getPublicStories = async (req, res, next) =>{
-        const sortby = req.query.sortby || "Recent"
-        let sortOption=-1
-        if (sortby==="Oldest"){
-            sortOption = +1
-        }
-        const retrievedStories = await Story.find({ status: "public" })
+const getPublicStories = async (req, res, next) => {
+    const sortby = req.query.sortby || "Recent"
+    sortOption = sortby === "Oldest" ? 1 : -1
+    const ids = req.user.liked
+    let retrievedStories = []
+    if (sortby === "You Liked") {
+        retrievedStories = await Story.find({ status: "public",  '_id': {$in: ids}  })
         .sort({ updatedAt: sortOption })
         .lean()
         .populate("user")
         .exec()
+    } else {
+        retrievedStories = await Story.find({ status: "public" })
+            .sort({ updatedAt: sortOption })
+            .lean()
+            .populate("user")
+            .exec()
+    }
     retrievedStories.forEach((story) => {
         story.body = processText(story.body, 200)
         story.shortTitle = processText(story.title, 25)
-        story.editIcon =
-            story.user._id.equals(req.user._id) ? "" : "hidden"
+        story.editIcon = story.user._id.equals(req.user._id) ? "" : "hidden"
         story.storyID = story._id
         story.updatedAt = formatTime(story.updatedAt)
     })
     //creating an array of promises to get the number of likes for each story
-    const likePromises = retrievedStories.map(el=>{
-        return User.find({liked: el._id}).countDocuments()
+    const likePromises = retrievedStories.map((el) => {
+        return User.find({ liked: el._id }).countDocuments()
     })
 
     const likesArray = await Promise.all(likePromises)
-    for (let i =0; i<retrievedStories.length; i++){
+    for (let i = 0; i < retrievedStories.length; i++) {
         retrievedStories[i].likes = likesArray[i]
     }
-    if (sortby==="Most Likes"){
-        retrievedStories.sort((a,b)=>{
-            return a.likes>b.likes ? -1 : 1
+    if (sortby === "Most Likes") {
+        retrievedStories.sort((a, b) => {
+            return a.likes > b.likes ? -1 : 1
         })
     }
-    if (sortby==="Least Likes"){
-        retrievedStories.sort((a,b)=>{
-            return a.likes>b.likes ? 1 : -1
+    if (sortby === "Least Likes") {
+        retrievedStories.sort((a, b) => {
+            return a.likes > b.likes ? 1 : -1
         })
     }
-    req.retrievedStories=retrievedStories
+    req.retrievedStories = retrievedStories
     req.sortby = sortby
     next()
 }
-
-
 
 /*
 getting PUBLIC STORIES using middleware getPublicStories
@@ -89,75 +93,82 @@ getting PUBLIC STORIES using middleware getPublicStories
 router.get("/", ensureAuth, getPublicStories, async (req, res) => {
     try {
         let showfile = "index.ejs"
-        if (req.query.view ==="list"){
+        if (req.query.view === "list") {
             showfile = "indexlist.ejs"
         }
         res.render(showfile, {
             retrievedStories: req.retrievedStories,
             user: req.user,
-            sortby: req.sortby
+            sortby: req.sortby,
         })
     } catch (error) {
         console.log(error)
     }
 })
 
-
-
-
 //Editing a story
-router.get("/edit/:id", ensureAuth, async (req, res) =>{
-    const story = await Story.findOne({_id: req.params.id}).populate("user").lean()
-    if(!story){
+router.get("/edit/:id", ensureAuth, async (req, res) => {
+    const story = await Story.findOne({ _id: req.params.id })
+        .populate("user")
+        .lean()
+    if (!story) {
         return res.render("error/404")
     }
-    if (!story.user._id.equals(req.user._id)){
+    if (!story.user._id.equals(req.user._id)) {
         return res.redirect("/stories")
     }
-    res.render("./partials/edit.ejs", {story: story})
+    res.render("./partials/edit.ejs", { story: story })
 })
 //Viewing a story
-router.get("/:id", ensureAuth, async (req, res) =>{
-    const story = await Story.findOne({_id: req.params.id}).populate("user").lean()
-    if(!story){
+router.get("/:id", ensureAuth, async (req, res) => {
+    const story = await Story.findOne({ _id: req.params.id })
+        .populate("user")
+        .lean()
+    if (!story) {
         return res.render("error/404")
     }
-    if (!story.user._id.equals(req.user._id) && story.status !== "public"){
+    if (!story.user._id.equals(req.user._id) && story.status !== "public") {
         return res.redirect("/stories")
     }
     story.editIcon = story.user._id.equals(req.user._id) ? "" : "hidden"
-    res.render("viewstory.ejs", {story: story, formatTimeDateOnly: formatTimeDateOnly})
+    res.render("viewstory.ejs", {
+        story: story,
+        formatTimeDateOnly: formatTimeDateOnly,
+    })
 })
 //update story
-router.put('/:id', ensureAuth, async(req, res)=>{
+router.put("/:id", ensureAuth, async (req, res) => {
     req.body.user = req.user.id
     try {
-        await Story.findOneAndUpdate({_id: req.params.id}, req.body)
-        res.redirect("/dashboard")   
+        await Story.findOneAndUpdate({ _id: req.params.id }, req.body)
+        res.redirect("/dashboard")
     } catch (error) {
         console.log(error)
     }
 })
 
 //delete story
-router.delete('/:id', ensureAuth, async(req, res)=>{                                                                           
+router.delete("/:id", ensureAuth, async (req, res) => {
     try {
-        await Story.findByIdAndDelete({_id: req.params.id})
-        res.redirect("/dashboard") 
+        await Story.findByIdAndDelete({ _id: req.params.id })
+        res.redirect("/dashboard")
     } catch (error) {
         console.log(error)
     }
-   
 })
 
 //Update status of story
-router.patch('/:id', ensureAuth, async(req, res)=>{
+router.patch("/:id", ensureAuth, async (req, res) => {
     const id = req.params.id
     const status = req.body.status
     try {
-        await Story.findByIdAndUpdate({_id:id}, {status: status}, {timestamps: false})//prevents an updatedAt timestamp for this
-        res.redirect("/dashboard") 
-    } catch(error){
+        await Story.findByIdAndUpdate(
+            { _id: id },
+            { status: status },
+            { timestamps: false }
+        ) //prevents an updatedAt timestamp for this
+        res.redirect("/dashboard")
+    } catch (error) {
         console.log(error)
     }
 })
