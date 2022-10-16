4bbd1a9b4a0ace55e799c1c2a2ad96cab645061f
SD <sourish.d@gmail.com>
2022-10-16 15:13:10 -0500

    admin page route protected with ensureAuthAdmin

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,6 +8,16 @@ const ensureAuth = (req, res, next) =>{
     }
 }
 
+const ensureAuthAdmin = (req, res, next) =>{
+    if (req.isAuthenticated() && req.user.role==="admin") {
+        // console.log("EnsureAuth Speaking. The user is:")
+        // console.log(req.user)
+        return next()
+    } else {
+        res.redirect('/')
+    }
+}
+
 const ensureGuest = (req, res, next) =>{
     if (req.isAuthenticated()){
         console.log(req.user.firstName)
@@ -20,4 +30,5 @@ const ensureGuest = (req, res, next) =>{
 }
 
 module.exports.ensureAuth = ensureAuth
+module.exports.ensureAuthAdmin = ensureAuthAdmin
 module.exports.ensureGuest = ensureGuest
--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,12 @@
 const express = require("express")
 const router = express.Router()
-const { ensureAuth, ensureGuest } = require("../middleware/auth")
+const { ensureAuth, ensureGuest, ensureAuthAdmin } = require("../middleware/auth")
 const bcrypt = require('bcrypt')
 const Story = require("../models/Story")
 const User = require("../models/User")
 
 
-router.get("/admin-dashboard", ensureAuth, (req, res) => {
+router.get("/admin-dashboard", ensureAuthAdmin, (req, res) => {
     res.render("./admin/admin-dashboard.ejs")
 })
 
