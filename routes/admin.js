27b3c5564baf5d6fe1c09e50ef9b21469b96ba8f
SD <sourish.d@gmail.com>
2022-10-16 21:09:56 -0500

    admin dashboard return button added to admin-users

--- routes/admin.js
+++ routes/admin.js
@@ -7,14 +7,14 @@ const User = require("../models/User")
 
 
 router.get("/dashboard", ensureAuthAdmin, (req, res) => {
-    res.render("./admin/admin-dashboard.ejs", {user: req.user})
+    res.status(200).render("./admin/admin-dashboard.ejs", {user: req.user})
 })
 
 router.get("/users", ensureAuthAdmin, async (req, res) =>{
     try {
         const users = await User.find({})
         console.log(users.length)
-        res.status(200).render('./admin/admin-users.ejs')
+        res.status(200).render('./admin/admin-users.ejs', {users})
 
     } catch (error) {
         console.log(error)
