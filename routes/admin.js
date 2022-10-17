e9471950db229a086b4773531fde5a5f4c595b68
SD <sourish.d@gmail.com>
2022-10-16 20:05:40 -0500

    users retrieved and redirecting to admin-users

--- routes/admin.js
+++ routes/admin.js
@@ -10,4 +10,28 @@ router.get("/dashboard", ensureAuthAdmin, (req, res) => {
     res.render("./admin/admin-dashboard.ejs", {user: req.user})
 })
 
+router.get("/users", ensureAuthAdmin, async (req, res) =>{
+    try {
+        const users = await User.find({})
+        console.log(users.length)
+        res.status(200).render('./admin/admin-users.ejs')
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).render("./error/500.ejs", { error })
+    }
+})
+
+router.get("/stories", ensureAuthAdmin, async (req, res) =>{
+    try {
+        const stories = await Story.find({})
+        console.log(stories.length)
+        res.status(200).render('./admin/admin-users.ejs')
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).render("./error/500.ejs", { error })
+    }
+})
+
 module.exports = router
