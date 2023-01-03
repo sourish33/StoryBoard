992b10646b30cdd380ccc8584026e3bf7c75a6d7
SD <sourish.d@gmail.com>
2023-01-03 06:29:24 -0600

    gets number of stories written by each author in the admin-user page

--- routes/admin.js
+++ routes/admin.js
@@ -1,40 +1,57 @@
 const express = require("express")
 const router = express.Router()
 const { ensureAuthAdmin } = require("../middleware/auth")
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt")
 const Story = require("../models/Story")
 const User = require("../models/User")
-const moment = require('moment');
-
+const moment = require("moment")
+const { promiseImpl } = require("ejs")
 
 router.get("/dashboard", ensureAuthAdmin, (req, res) => {
-    res.status(200).render("./admin/admin-dashboard.ejs", {user: req.user})
+    res.status(200).render("./admin/admin-dashboard.ejs", { user: req.user })
 })
 
-router.get("/users", ensureAuthAdmin, async (req, res) =>{
+router.get("/users", ensureAuthAdmin, async (req, res) => {
     try {
         const users = await User.find({}).lean().exec()
+        //create querries for getting number of stories for each user
+        let storyQueries = []
+        for (let user of users) {
+            let storiesQuery = Story.find({ user: user._id }).lean().exec()
+            storyQueries.push(storiesQuery)
+        }
+        const storyList = await Promise.all(storyQueries)
+        const storyNums = storyList.map((el) => el.length)
+        // console.log(storyNums)
         //can't mutate users for some reason so creating a new array usersDateFormatted
         let usersDateFormatted = []
-        for (let user of users) {
-            const formattedDate = moment(user.createdAt).format('YYYY-MM-DD');
-            const shortUser = {_id: user._id, email: user.email, firstName: user.firstName, lastName: user.lastName, liked: user.liked.length, joinDate: formattedDate}
+        for (let i = 0; i < users.length; i++) {
+            const formattedDate = moment(users[i].createdAt).format("YYYY-MM-DD")
+            const shortUser = {
+                _id: users[i]._id,
+                email: users[i].email,
+                firstName: users[i].firstName,
+                lastName: users[i].lastName,
+                wrote: storyNums[i],
+                liked: users[i].liked.length,
+                joinDate: formattedDate,
+            }
             usersDateFormatted.push(shortUser)
         }
-        res.status(200).render('./admin/admin-users.ejs', {users: usersDateFormatted})
+        res.status(200).render("./admin/admin-users.ejs", {
+            users: usersDateFormatted,
+        })
     } catch (error) {
         console.log(error)
         res.status(500).render("./error/500.ejs", { error })
     }
 })
 
-
-router.get("/stories", ensureAuthAdmin, async (req, res) =>{
+router.get("/stories", ensureAuthAdmin, async (req, res) => {
     try {
         const stories = await Story.find({})
         console.log(stories.length)
-        res.status(200).render('./admin/admin-users.ejs')
-
+        res.status(200).render("./admin/admin-users.ejs")
     } catch (error) {
         console.log(error)
         res.status(500).render("./error/500.ejs", { error })
