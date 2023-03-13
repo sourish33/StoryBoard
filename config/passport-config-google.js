4550a5576ac8cdf5439d029dfcbfb5f7f1e5fa53
SD <sourish.d@gmail.com>
2023-03-13 14:16:49 -0500

    changed google callbackurl to auth/google/callback as before

--- config/passport-config-google.js
+++ config/passport-config-google.js
@@ -32,8 +32,7 @@ const initializePassport  = function(passport) {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        // callbackURL: '/auth/google/callback'
-        callbackURL: 'http://wild-erin-woodpecker-cape.cyclic.app/auth/google/callback' 
+        callbackURL: '/auth/google/callback'
     }, authenticateUser))
 
     passport.serializeUser((user,done)=>{
